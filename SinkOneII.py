0dfe19e4af5515810b8cf3e0ad6da5b888a71f90
silver2row <silver1row@cox.net>
2018-12-06 00:59:05 +0000

    adding to software

--- SinkOneII.py
+++ SinkOneII.py
@@ -4,80 +4,144 @@ import time
 # from help on the googlegroups section of bbb.io!
 
 class BrushedDC(object):
-     def __init__(self, p1):
-         self.p1 = p1
-         #self.p2 = p2
-         GPIO.setup(self.p1, GPIO.OUT)
-         #GPIO.setup(self.p2, GPIO.OUT)
+
+     def __init__(self, m1):
+         self.m1 = m1
+         #self.m2 = m2
+         GPIO.setup(self.m1, GPIO.OUT)
+         #GPIO.setup(self.m2, GPIO.OUT)
          self.stop()
+
      def stop(self):
-         GPIO.output(self.p1, GPIO.LOW)
-         #GPIO.output(self.p2, GPIO.LOW)
+         GPIO.output(self.m1, GPIO.LOW)
+         #GPIO.output(self.m2, GPIO.LOW)
+
      def forward(self):
          self.stop()     #safety to avoid shock of reversals
-         GPIO.output(self.p1, GPIO.HIGH)
-         #GPIO.output(self.p2, GPIO.LOW)
+         GPIO.output(self.m1, GPIO.HIGH)
+         #GPIO.output(self.m2, GPIO.LOW)
+
      def reverse(self):
          self.stop()
-         GPIO.output(self.p1, GPIO.LOW)
-         #GPIO.output(self.p2, GPIO.HIGH)
+         GPIO.output(self.m1, GPIO.LOW)
+         #GPIO.output(self.m2, GPIO.HIGH)
+
+class BrushedDCII(object):
+
+    def __init__(self, m2):
+        self.m2 = m2
+        GPIO.setup(self.m2, GPIO.OUT)
+        self.stop()
+
+    def stop(self):
+        GPIO.output(self.m2, GPIO.LOW)
+
+    def forward(self):
+        GPIO.output(self.m2, GPIO.LOW)
+
+    def reverse(self):
+        GPIO.output(self.m2, GPIO.HIGH)
 
 class Car(object):
-     def __init__(self, leftMotor):
-         self.leftMotor = leftMotor
-         #self.rightMotor = rightMotor
-         self.stop()
-     def stop(self, wait=0):
-         self.leftMotor.stop()
-         #self.rightMotor.stop()
-         time.sleep(wait)
-     def forward(self, wait=0):
-         self.leftMotor.forward()
-         #self.rightMotor.forward()
-         time.sleep(wait)
-     def reverse(self, wait=0):
-         self.leftMotor.reverse()
-         #self.rightMotor.reverse()
-         time.sleep(wait)
-     def leftTurnForward(self, wait=0):
-         self.leftMotor.stop()
-         #self.rightMotor.forward()
-         time.sleep(wait)
-     def rightTurnForward(self, wait=0):
-         self.leftMotor.forward()
-         #self.rightMotor.stop()
-         time.sleep(wait)
-     def leftTurnReverse(self, wait=0):
-         self.leftMotor.stop()
-         #self.rightMotor.reverse()
-         time.sleep(wait)
-     def rightTurnReverse(self, wait=0):
-         self.leftMotor.reverse()
-         #self.rightMotor.stop()
-         time.sleep(wait)
-     def leftPivot(self, wait=0):
-         self.leftMotor.reverse()
-         #self.rightMotor.forward()
-         time.sleep(wait)
-     def rightPivot(self, wait=0):
-         self.leftMotor.forward()
-         #self.rightMotor.reverse()
-         time.sleep(wait)
+    def __init__(self, leftMotor):
+        self.leftMotor = leftMotor
+        #self.rightMotor = rightMotor
+        self.stop()
+    def stop(self, wait=0):
+        self.leftMotor.stop()
+        #self.rightMotor.stop()
+        time.sleep(wait)
+    def forward(self, wait=0):
+        self.leftMotor.forward()
+        #self.rightMotor.forward()
+        time.sleep(wait)
+    def reverse(self, wait=0):
+        self.leftMotor.reverse()
+        #self.rightMotor.reverse()
+        time.sleep(wait)
+    def leftTurnForward(self, wait=0):
+        self.leftMotor.stop()
+        #self.rightMotor.forward()
+        time.sleep(wait)
+    def rightTurnForward(self, wait=0):
+        self.leftMotor.forward()
+        #self.rightMotor.stop()
+        time.sleep(wait)
+    def leftTurnReverse(self, wait=0):
+        self.leftMotor.stop()
+        #self.rightMotor.reverse()
+        time.sleep(wait)
+    def rightTurnReverse(self, wait=0):
+        self.leftMotor.reverse()
+        #self.rightMotor.stop()
+        time.sleep(wait)
+    def leftPivot(self, wait=0):
+        self.leftMotor.reverse()
+        #self.rightMotor.forward()
+        time.sleep(wait)
+    def rightPivot(self, wait=0):
+        self.leftMotor.forward()
+        #self.rightMotor.reverse()
+        time.sleep(wait)
 
+class CarII(object):
 
-myCar = Car(leftMotor=BrushedDC(p1="P9_42"))
-             #rightMotor=BrushedDC(p1="P9_12", p2="P9_15"))
+    def __init__(self, rightMotor):
+        self.rightMotor = rightMotor
+        self.stop()
+    def stop(self, wait=0):
+        self.rightMotor.stop()
+        time.sleep(wait)
+    def forward(self, wait=0):
+        self.rightMotor.forward()
+        time.sleep(wait)
+    def reverse(self, wait=0):
+        self.rightMotor.reverse()
+        time.sleep(wait)
+    def leftTurnForward(self, wait=0):
+        self.rightMotor.forward()
+        time.sleep(wait)
+    def rightTurnForward(self, wait=0):
+        self.rightMotor.stop()
+        time.sleep(wait)
+    def leftTurnReverse(self, wait=0):
+        self.rightMotor.reverse()
+        time.sleep(wait)
+    def rightTurnReverse(self, wait=0):
+        self.rightMotor.stop()
+        time.sleep(wait)
+    def leftPivot(self, wait=0):
+        self.rightMotor.forward()
+        time.sleep(wait)
+    def rightPivot(self, wait=0):
+        self.rightMotor.reverse()
+        time.sleep(wait)
 
-myCar.forward(10)
-myCar.leftPivot(3)
+myCar = Car(leftMotor=BrushedDC(m1="P9_42"))
+
+myCarII = CarII(rightMotor=BrushedDCII(m2="P9_41"))
+
 myCar.forward(5)
-myCar.rightTurnReverse(3)
-myCar.reverse(10)
-myCar.stop(10)
-myCar.rightPivot(30)
+myCar.leftPivot(5)
 myCar.forward(5)
-myCar.rightTurnForward(15)
+myCar.rightTurnReverse(5)
+myCar.reverse(5)
+myCar.stop(2)
+myCar.rightPivot(5)
+myCar.forward(5)
+myCar.rightTurnForward(5)
 myCar.stop()
+
+myCarII.forward(5)
+myCarII.leftPivot(5)
+myCarII.forward(5)
+myCarII.rightTurnReverse(5)
+myCarII.reverse(5)
+myCarII.stop(2)
+myCarII.rightPivot(5)
+myCarII.forward(5)
+myCarII.rightTurnForward(5)
+myCarII.stop()
 
 GPIO.cleanup()
 
