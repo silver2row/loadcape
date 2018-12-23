f03bab587f2e9eeaf14a438d2039d4a9a927f403
silver2row <silver1row@cox.net>
2018-12-23 20:43:42 +0000

    Making more ideas a reality

--- UseSinkerOneII.py
+++ UseSinkerOneII.py
@@ -6,16 +6,10 @@ import time
 m1 = "P9_42"
 
 while True:
+    GPIO.output(m1, GPIO.HIGH)
+    time.sleep(5)
+    print("Moving on down the road!")
 
-    if m1 == 1:
-        if Left.forward == 8:
-            Left.forward = 8
-            time.sleep(2)
-
-    else:
-        if Left.forward == 9:
-            Left.reverse = 8
-            time.sleep(2)
-            print("Um...")
-
-left.stop()
+    GPIO.output(m1, GPIO.LOW)
+    time.sleep(10)
+    print("Stopping this boat!")
